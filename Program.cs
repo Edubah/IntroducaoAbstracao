767fd2eda42ed90e93da2695dc28dbb808683c89
Eduardo <edugc2014@hotmail.com>
2025-04-10 14:50:32 -0300

    Término do Módulo

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using IntroducaoAbstracao.Models;
+
+ContaCorrente c1 = new ContaCorrente(123, 1000);
+
+c1.ExibirSaldo();
+c1.Sacar(5000);
+c1.ExibirSaldo();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new Pessoa();
+// p1.Nome = "Eduardo";
+// p1.Idade = 27;
+
+// p1.Apresentar();
